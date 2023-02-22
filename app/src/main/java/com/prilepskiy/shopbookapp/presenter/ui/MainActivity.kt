485c34b22d4225beaa67ed879e9a542e51b8e70d
Prilepskiy_ae <alexey.prilepskiy@btb.dev>
2023-02-22 23:34:28 +0700

    trening PageIndicator

--- app/src/main/java/com/prilepskiy/shopbookapp/presenter/ui/MainActivity.kt
+++ app/src/main/java/com/prilepskiy/shopbookapp/presenter/ui/MainActivity.kt
@@ -5,6 +5,10 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -16,10 +20,14 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
 import com.prilepskiy.shopbookapp.R
@@ -117,9 +125,11 @@ class MainActivity : ComponentActivity() {
 
 
         }
+        PageIndicatorSample()
         LaunchedEffect(true) {
             repeat(Int.MAX_VALUE) {
                 delay(5500)
+
                 lazyListState.animateScrollToItem( it % list.size)
             }
         }
@@ -150,6 +160,115 @@ class MainActivity : ComponentActivity() {
             })
 
 
+    }
+
+    @Composable
+    fun PageIndicatorSample() {
+        val numberOfPages = 3
+        val (selectedPage, setSelectedPage) = remember {
+            mutableStateOf(0)
+        }
+
+        // NEVER use this, this is just for example
+        LaunchedEffect(
+            key1 = selectedPage,
+        ) {
+            delay(3000)
+            setSelectedPage((selectedPage + 1) % numberOfPages)
+        }
+
+        PageIndicator(
+            numberOfPages = numberOfPages,
+            selectedPage = selectedPage,
+            defaultRadius = 60.dp,
+            selectedLength = 120.dp,
+            space = 30.dp,
+            animationDurationInMillis = 1000,
+        )
+    }
+
+    @Composable
+    fun PageIndicator(
+        numberOfPages: Int,
+        modifier: Modifier = Modifier,
+        selectedPage: Int = 0,
+        selectedColor: Color = Color.Blue,
+        defaultColor: Color = Color.LightGray,
+        defaultRadius: Dp = 20.dp,
+        selectedLength: Dp = 60.dp,
+        space: Dp = 30.dp,
+        animationDurationInMillis: Int = 300,
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(space),
+            modifier = modifier,
+        ) {
+            for (i in 0 until numberOfPages) {
+                val isSelected = i == selectedPage
+                PageIndicatorView(
+                    isSelected = isSelected,
+                    selectedColor = selectedColor,
+                    defaultColor = defaultColor,
+                    defaultRadius = defaultRadius,
+                    selectedLength = selectedLength,
+                    animationDurationInMillis = animationDurationInMillis,
+                )
+            }
+        }
+    }
+    @Composable
+    fun PageIndicatorView(
+        isSelected: Boolean,
+        selectedColor: Color,
+        defaultColor: Color,
+        defaultRadius: Dp,
+        selectedLength: Dp,
+        animationDurationInMillis: Int,
+        modifier: Modifier = Modifier,
+    ) {
+
+        val color: Color by animateColorAsState(
+            targetValue = if (isSelected) {
+                selectedColor
+            } else {
+                defaultColor
+            },
+            animationSpec = tween(
+                durationMillis = animationDurationInMillis,
+            )
+        )
+        val width: Dp by animateDpAsState(
+            targetValue = if (isSelected) {
+                selectedLength
+            } else {
+                defaultRadius
+            },
+            animationSpec = tween(
+                durationMillis = animationDurationInMillis,
+            )
+        )
+
+        Canvas(
+            modifier = modifier
+                .size(
+                    width = width,
+                    height = defaultRadius,
+                ),
+        ) {
+            drawRoundRect(
+                color = color,
+                topLeft = Offset.Zero,
+                size = Size(
+                    width = width.toPx(),
+                    height = defaultRadius.toPx(),
+                ),
+                cornerRadius = CornerRadius(
+                    x = defaultRadius.toPx(),
+                    y = defaultRadius.toPx(),
+                ),
+            )
+        }
     }
 
 }
